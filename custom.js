13f008b09b7cf2210c7d0485ca2e26e45304ae0f
webworld420 <76555387+webworld420@users.noreply.github.com>
2022-06-16 16:25:27 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+    $('.module-area .module1').click(function(){
+        $('.module1 p').toggle(100);
+
+        $('.module1 .carret span').toggle();
+    })
+    $('.module-area .module2').click(function(){
+        $('.module2 p').toggle(100);
+
+        $('.module2 .carret span').toggle();
+    })
+    $('.module-area .module3').click(function(){
+        $('.module3 p').toggle(100);
+
+        $('.module3 .carret span').toggle();
+    })
+    $('.module-area .module4').click(function(){
+        $('.module4 p').toggle(100);
+
+        $('.module4 .carret span').toggle();
+    })
+    $('.module-area .module5').click(function(){
+        $('.module5 p').toggle(100);
+
+        $('.module5 .carret span').toggle();
+    })
+
+
+
+
+    $('.Certification-Process .certificate1').click(function(){
+        $('.Certification-Process .certificate1 .text li').toggle(100);
+
+        $('.Certification-Process .certificate1 .carret span').toggle();
+    })
+
+    $('.Certification-Process .certificate2').click(function(){
+        $('.Certification-Process .certificate2 .text li').toggle(100);
+
+        $('.Certification-Process .certificate2 .carret span').toggle();
+    })
+
+    $('.Certification-Process .certificate3').click(function(){
+        $('.Certification-Process .certificate3 .text li').toggle(100);
+
+        $('.Certification-Process .certificate3 .carret span').toggle();
+    })
+
+
+
+
+
+
+
+
+
+  
+   $(".faq-box .qus-ans1").click(function(){
+        $('.qus-ans1 .ans p').toggle(200);
+        $(".qus-ans1 .pluse").children('span').fadeToggle();
+    });
+
+    $(".faq-box .qus-ans2").click(function(){
+        $('.qus-ans2 .ans p').toggle(200);
+        $(".qus-ans2 .pluse").children('span').fadeToggle();
+    });
+      $(".faq-box .qus-ans3").click(function(){
+        $('.qus-ans3 .ans p').toggle(200);
+        $(".qus-ans3 .pluse").children('span').fadeToggle();
+    });
+    $(".faq-box .qus-ans4").click(function(){
+        $('.qus-ans4 .ans p').toggle(200);
+        $(".qus-ans4 .pluse").children('span').fadeToggle();
+    });
+    
+    $(".faq-box .qus-ans5").click(function(){
+        $('.qus-ans5 .ans p').toggle(200);
+        $(".qus-ans5 .pluse").children('span').fadeToggle();
+    });
+     $(".faq-box .qus-ans6").click(function(){
+        $('.qus-ans6 .ans p').toggle(200);
+        $(".qus-ans6 .pluse").children('span').fadeToggle();
+    });
+
+   $(".faq-box .qus-ans7").click(function(){
+        $('.qus-ans7 .ans p').toggle(200);
+        $(".qus-ans7 .pluse").children('span').fadeToggle();
+    });
